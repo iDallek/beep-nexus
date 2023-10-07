8d4ef4220c15cbd65c6fc1869ec6f194869b1f1d
Gabriel Carvalho Barbosa <gabrielcarvalho466@gmail.com>
2023-10-07 11:00:50 -0300

    feat: add indefier to get type of datasheet

--- src/components/ReadXlsx.vue
+++ src/components/ReadXlsx.vue
@@ -34,19 +34,65 @@
     import { read, utils } from 'xlsx'
     import moment from 'moment'
 
-    async function onChange(e: any) : Promise<void> {
-        console.log("Passou pela função 'onChange'")
+    function identifyDatasheet(firstRow): string {
+        const typesOfDatasheet = [
+            { 'Reincidencia': ['Reincidências'] },
+            { 'Estoque': ['Código', 'Nome',	'Complemento', 'Estoque', 	'Estoque Indisponível', 'Dias sem venda', 'Última venda', 'Última venda', 'Última Compra', 'Última Compra', 'Classe de Produto', 'Classe de Produto Raiz', 'Comprador', 'Curva ABC', 'Preço de Venda', 'Qtde Venda/Dia', 'Vida útil', 'Cadeia', 'Setor', 'Setor herdado', 'Fornecedor', 'Gerenciado', 'Ativo', 'Resíduo', 'Peso Variável', 'Custo', 'Alteração de Preço', 'Alteração de Preço', 'Verificador']}
+        ]
 
+        let datasheetType
+        typesOfDatasheet.forEach(element => {
+            // Refatorar, a linha abaixo demonstar apenas se o typesOfDatasheet tiver uma unica string em cada key
+            const keyValue = element[`${Object.keys(element)}`][0]
+            const keyName = Object.keys(element)[0]
+
+            const notAvailable : string[] = [];
+            const emptyVals: string[] = [];
+
+            element[keyName].forEach((key) => {
+                if (!firstRow.hasOwnProperty(key)) {
+                    notAvailable.push(key);
+                } else {
+                    if (firstRow[key] == null || firstRow[key] == "") {
+                        emptyVals.push(key);
+                    }
+                }
+            })
+
+            if (notAvailable.length == 0) {
+                return keyName
+            } else {
+                return "undefinedType"
+            }
+        });
+
+        return datasheetType
+    }
+
+    async function onChange(e: any) : Promise<void> {
         const files = await e.srcElement?.files
+        const xlsxData = {
+            date: `${moment().format('L')} ${moment().format('dddd')}`
+        }
         
         for (const file of files) {
             const content = await file.arrayBuffer()
-
-            console.log(content)
             const wb = read(content)
 
-            console.log(`${moment().format('L')} ${moment().format('dddd')}`, utils.sheet_to_json(wb.Sheets[wb.SheetNames[0]]))
+            const data = utils.sheet_to_json(wb.Sheets[wb.SheetNames[0]])
+            const type = identifyDatasheet(data[0])
+
+            console.log(type)
+
+            if (type != "undefinedType") {
+                xlsxData["datasheetType"] = type
+                xlsxData["data"] = data
+            } else {
+                xlsxData["datasheetType"] = type
+            }
         }
+
+        // console.log(xlsxData)
     }
 
 export default {
