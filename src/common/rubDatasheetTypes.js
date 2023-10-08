c4b5b551a593ff2f72c871a6f0fef698a5cd200f
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2023-10-08 08:59:05 -0300

    refactor: changes the location typesDatasheet constant to its own file

---
+++ src/common/rubDatasheetTypes.js
@@ -0,0 +1,35 @@
+const typesOfDatasheet = [
+  { Reincidencia: ['Reincidências'] },
+  {
+    Estoque: [
+      'Código',
+      'Nome',
+      'Complemento',
+      'Estoque',
+      'Estoque Indisponível',
+      'Dias sem venda',
+      'Última venda',
+      'Última Compra',
+      'Classe de Produto',
+      'Classe de Produto Raiz',
+      'Comprador',
+      'Curva ABC',
+      'Preço de Venda',
+      'Qtde Venda/Dia',
+      'Vida útil',
+      'Cadeia',
+      'Setor',
+      'Setor herdado',
+      'Fornecedor',
+      'Gerenciado',
+      'Ativo',
+      'Resíduo',
+      'Peso Variável',
+      'Custo',
+      'Alteração de Preço',
+      'Verificador'
+    ]
+  }
+]
+
+export default typesOfDatasheet
--- src/components/ReadXlsx.vue
+++ src/components/ReadXlsx.vue
@@ -25,41 +25,10 @@
 <script lang="ts">
 import { read, utils } from 'xlsx'
 import moment from 'moment'
+import typesOfDatasheet from '../common/rubDatasheetTypes'
 
 function identifyDatasheet(firstRow: any): string {
-  const typesOfDatasheet = [
-    { Reincidencia: ['Reincidências'] },
-    {
-      Estoque: [
-        'Código',
-        'Nome',
-        'Complemento',
-        'Estoque',
-        'Estoque Indisponível',
-        'Dias sem venda',
-        'Última venda',
-        'Última Compra',
-        'Classe de Produto',
-        'Classe de Produto Raiz',
-        'Comprador',
-        'Curva ABC',
-        'Preço de Venda',
-        'Qtde Venda/Dia',
-        'Vida útil',
-        'Cadeia',
-        'Setor',
-        'Setor herdado',
-        'Fornecedor',
-        'Gerenciado',
-        'Ativo',
-        'Resíduo',
-        'Peso Variável',
-        'Custo',
-        'Alteração de Preço',
-        'Verificador'
-      ]
-    }
-  ]
+  const types = typesOfDatasheet
 
   // console.log(firstRow)
 
@@ -67,7 +36,7 @@ function identifyDatasheet(firstRow: any): string {
   let keyName = ""
   let datasheetType = "undefinedType"
 
-  for (const el of typesOfDatasheet) {
+  for (const el of types) {
     keyName = Object.keys(el)[0]
     let matchLoops = 0
 
