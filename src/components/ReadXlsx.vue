e51bce0078be28df760a45d82d137c021753ee6b
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2023-10-08 09:05:38 -0300

    refactor: remove the counter and change the size of the uploaded file

--- src/components/ReadXlsx.vue
+++ src/components/ReadXlsx.vue
@@ -2,26 +2,21 @@
   <div>
     <v-file-input
       accept=".xlsx"
-      counter
       label="Escolha o arquivo"
-      multiple
       :show-size="1000"
       @change="onChange"
     >
       <template v-slot:selection="{ fileNames }">
         <template v-for="(fileName, index) in fileNames" :key="fileName">
-          <v-chip v-if="index < 2" label size="small" class="me-2">
+          <v-chip v-if="index < 2" label class="me-2">
             {{ fileName }}
           </v-chip>
-
-          <span v-else-if="index === 2" class="text-overline text-grey-darken-3 mx-2">
-            <!-- +{{ files.length - 2 }} File(s) -->
-          </span>
         </template>
       </template>
     </v-file-input>
   </div>
 </template>
+
 <script lang="ts">
 import { read, utils } from 'xlsx'
 import moment from 'moment'
@@ -29,8 +24,6 @@ import typesOfDatasheet from '../common/rubDatasheetTypes'
 
 function identifyDatasheet(firstRow: any): string {
   const types = typesOfDatasheet
-
-  // console.log(firstRow)
 
   const notAvailable: string[] = []
   let keyName = ""
@@ -45,8 +38,6 @@ function identifyDatasheet(firstRow: any): string {
         matchLoops += 1
       }
     })
-
-    // console.log(matchLoops, Object.keys(el[keyName]).length)
 
     if (matchLoops == el[keyName].length) {
       datasheetType = keyName
